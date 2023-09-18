74dbf7ec5941817e0118195498476b2ae758d836
yface504 <even504@hotmail.com.tw>
2023-09-18 21:14:44 +0800

    登入

--- rt_admin/work/views.py
+++ rt_admin/work/views.py
@@ -1,4 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from work import models
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate,login,logout
+from django.core.paginator import Paginator,EmptyPage,PageNotAnInteger
 from .models import Publisher
 # Create your views here.
 
@@ -8,10 +12,23 @@ def index(request):
     return render(request,'index.html')
 
 
-def login(request):
-    pass
-# 返回響應
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+        user = authenticate(username=username,password=password)
+        # 用authenticate判断用户名密码是否正确
+        if user:
+            login(request,user)
+            return redirect('index')
+        else:
+            msg='帐密错误！'
+            return render(request,'login.html',locals())
     return render(request,'login.html')
+
+def log_out(request):
+    logout(request)
+    return redirect('/')
 
 def publisher(request):
     publishers = Publisher.objects.all()
