af7548eb9785de45cc68fc9f200f9c1473186dd9
even504@hotmail.com.tw <even504@hotmail.com.tw>
2023-10-14 08:17:35 +0800

    台灣報表轉換完成

--- rt_admin/rt_admin/urls.py
+++ rt_admin/rt_admin/urls.py
@@ -1,13 +1,14 @@
 from django.contrib import admin
 from django.urls import path
 from work import views
+from work import view_update
 
 urlpatterns = [
     path('index/', views.index , name='index'),
     path('login/', views.login_view , name='login'),
     path('logout/', views.log_out_view , name='logout'),
     path('publisher/' ,views.publisher, name='publisher'),
-    path('update/' ,views.update, name='update'),
+    path('update/' ,view_update.update, name='update'),
     path('paper/' ,views.paper, name='paper'),
     path('admin/', admin.site.urls),
 ]
